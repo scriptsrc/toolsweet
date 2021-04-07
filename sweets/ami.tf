5514d1494630955068ee7ef3454f2d9477a04390
Patrick Kelley <patrickbarrettkelley@gmail.com>
2021-04-06 22:58:34 -0600

    Update ami.tf

--- sweets/ami.tf
+++ sweets/ami.tf
@@ -1,5 +1,6 @@
 # From: https://github.com/accurics/terrascan/blob/bda153e4aa8f4d457d0a82c5076d00439f082711/test/e2e/test_data/iac/aws/aws_ami_violation/main.tf
 # Should cause terrascan to complain
+# 123
 
 provider "aws" {
   region = "us-west-2"
